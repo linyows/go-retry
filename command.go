bad8094ebec35adf86d31ae2606788593dc410ca
linyows <linyows@gmail.com>
2015-11-22 16:42:38 +0900

    extract cli.go to command.go, ops.go and retry.go

---
+++ command.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func ExecCmd(c []string) int {
+	var cmd *exec.Cmd
+
+	if len(c) > 1 {
+		cmd = exec.Command(c[0], c[1:]...)
+	} else {
+		cmd = exec.Command(c[0])
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+
+	if exiterr, ok := err.(*exec.ExitError); ok {
+		if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+			return status.ExitStatus()
+		} else {
+			return ExitCodeError
+		}
+	}
+
+	return ExitCodeOK
+}
